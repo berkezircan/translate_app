eaa74e6ad24d549fc0464887ac6c95761e224458
berkezircan <berkezircan@gmail.com>
2021-12-02 18:11:55 +0300

    Add font awesome

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './styles/App.scss';
 function App() {
 	return (
 		<div className="App">
-			<div className="container">Test</div>
+			<div className="container">
+				<h1>Test</h1>
+				<p>Test</p>
+			</div>
 		</div>
 	);
 }
