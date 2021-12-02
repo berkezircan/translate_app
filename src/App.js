8d2ac87a95cdc8823a381e45d1069cb85eb297bd
berkezircan <berkezircan@gmail.com>
2021-12-02 18:05:17 +0300

    Add Variables

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './styles/App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">Test</div>
+		</div>
+	);
 }
 
 export default App;
