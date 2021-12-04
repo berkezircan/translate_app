6e7d538e560ae9a8235c9bb4d476f7e7a1aa894e
berkezircan <berkezircan@gmail.com>
2021-12-05 02:29:12 +0300

    Add Translate functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import { useState } from 'react';
+import translate from 'translate';
+// Styles
 import './styles/App.scss';
-
+// Components
 import TranslatedWord from './components/TranslatedWord';
 import TranslateWordResult from './components/TranslateWordResult';
 
 function App() {
+	const [word, setWord] = useState('');
+	const [translatedWord, setTranslatedWord] = useState('');
+
+	const translateWord = async (word) => {
+		translate.engine = 'google';
+		translate.key = 'AIzaSyD11ylXgBEAvMeaSYU1DFULsFZJmiRU1Dc';
+
+		const response = await translate('asdasd', 'tr');
+		console.log(1, response);
+	};
+
 	return (
 		<div className="App">
 			<div className="container">
 				<h1 className="display-1 title">Translate</h1>
 				<div className="translate-container">
-					<TranslatedWord />
+					<TranslatedWord word={word} setWord={setWord} />
 					<TranslateWordResult />
 				</div>
 			</div>
--- src/components/TranslatedWord.js
+++ src/components/TranslatedWord.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import TranslateButton from './TranslateButton';
 
-const TranslatedWord = () => {
+const TranslatedWord = ({ word, setWord }) => {
+	const onWordChange = (e) => {
+		setWord(e.target.value);
+	};
+
 	return (
 		<div className="translate-area">
 			<textarea
 				placeholder="Translate Here"
 				className="translate-input translate-item"
+				onChange={(e) => onWordChange(e)}
 			/>
 			<TranslateButton />
 		</div>
